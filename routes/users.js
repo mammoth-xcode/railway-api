e6c543798e5a9f8714d31709b41497a3be870908
mammoth-xcode <mammoth.xcode@gmail.com>
2025-05-19 16:33:46 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../db');
 
 router.get('/', (req, res) => {
-  db.query('SELECT * FROM users', (err, results) => {
+  db.query('SELECT * FROM customers', (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
     res.json(results);
   });
