66cad250f74d9319507c5de303b24045bef03f88
mammoth-xcode <mammoth.xcode@gmail.com>
2025-05-21 09:50:03 +0700

    index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/users', require('./routes/users'));
+app.use('/api/customers', require('./routes/customers'));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
